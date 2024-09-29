493178efdf06b5caf3f5118280eef8dce4b41fb7
Marcela Motta <marcelarmotta@gmail.com>
2024-09-29 16:36:38 +0200

    Eliminacion de archivos mal renombrados

--- Ejercicio7.sql
+++
@@ -1,8 +0,0 @@
-create table `Keepcoding.ivr_billing` as 
-SELECT ivr_id, billing_account_id
-  FROM (
-    SELECT ivr_id, billing_account_id,
-    ROW_NUMBER() OVER(PARTITION BY CAST(ivr_id as STRING) ORDER BY billing_account_id) AS rn
-    FROM `Keepcoding.Steps` 
-  )
- WHERE rn = 1
--- Ejercicio8.sql
+++
@@ -1,4 +0,0 @@
-create table `Keepcoding.ivr_masiva` as 
-SELECT distinct ivr_id,
-    MAX(CASE WHEN  module_name = 'AVERIA_MASIVA' THEN 1 ELSE 0 END) OVER (PARTITION BY CAST(ivr_id as STRING)) as masiva_lg
-FROM `Keepcoding.Modules`
